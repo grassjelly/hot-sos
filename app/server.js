6100ee849b81fcfdc5be3b13eabf2c1bdb83c5e9
Juan Jimeno <jimenojmm@gmail.com>
2018-10-21 09:02:23 +0800

    added changing of tree status

--- app/server.js
+++ app/server.js
@@ -4,6 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 //either use cloud service port or local port:3000
 var port = process.env.PORT || 8000;
+var keypress = require('keypress');
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
@@ -17,3 +18,30 @@ io.sockets.on('connection', function(socket) {
       console.log(data);
     });
 });
+
+keypress(process.stdin);
+console.log("x, y, z");
+process.stdin.on('keypress', function(ch, key) {
+  if (key && key.ctrl && key.name == 'c') {
+      process.exit();
+  }
+
+  switch (key.name) {
+      case 'z':
+        io.emit("status", "z");
+        break;
+
+      case 'x':
+        io.emit("status", "x");
+        break;
+
+      case 'c':
+        io.emit("status", "c");
+        break;
+  }
+
+  console.log(key.name);
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
